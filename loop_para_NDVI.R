e4b1e97bdfaf20bd77c95ea42bb8893317301735
Karlo Guidoni Martins <kguidonimartins@gmail.com>
2019-04-23 17:47:34 -0300

    Add files via upload

---
+++ loop_para_NDVI.R
@@ -0,0 +1,45 @@
+setwd("C:\\Users\\marcao\\Desktop\\Drones\\NDVI\\2018_MAIO")
+setwd("C:\\Users\\marcao\\Desktop\\Drones\\NDVI\\2018_JUNHO")
+setwd("C:\\Users\\marcao\\Desktop\\Drones\\NDVI\\2018_SETEMBRO")
+setwd("C:/Users/marcao/Desktop/Drones/2018_1019/jpeg/DR14/Calibrated_1/")
+
+
+ipak <- function(pkg){
+  new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
+  if(length(new.pkg))
+    install.packages(new.pkg, dependencies = TRUE)
+  sapply(pkg, require, character.only = TRUE)
+}
+
+# load spatial packages
+
+ipak(c("raster",
+       "rgdal",
+       "rgeos",
+       "RColorBrewer"))
+
+# turn off factors
+options(stringsAsFactors = FALSE)
+
+# list files 
+(fts <- list.files(path = ".", pattern = "*.JPG$"))
+
+# batch process
+for(i in seq_along(fts)){
+  # read file i
+  foto <- stack(x = fts[i])
+  # process file i
+  foto.br <- brick(foto)
+  # calculate NDVI
+  naip_ndvi <- (foto.br[[3]] - foto.br[[1]]) / (foto.br[[3]] + foto.br[[1]])
+  # plot NDVI
+  plot(naip_ndvi,
+       main = paste("EMAS", fts[i]),
+       axes = FALSE, box = FALSE)
+  # save NDVI
+  writeRaster(x = naip_ndvi, filename = paste0("NDVI_", gsub(pattern = ".JPG$", replacement = "", x = fts[i]), ".tiff"))
+}
+
+
+
+
